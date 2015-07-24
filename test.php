7e2d599052b6f15205e575bb2d58ea396cf21c4a
luoxiao <409494308@qq.com>
2015-07-24 16:56:57 +0800

    get message

--- test.php
+++ test.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
+<?php
+include 'config/config.php';
+require_once('leancloud/AV.php');
+?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title></title>
 </head>
 <body>
-	<?php
-if(isset($_COOKIE['test']))
-			echo $_COOKIE['test'];
-		else
-			setcookie('test','heheda',time()+60*60);
+<?php
+	$query = new leancloud\AVQuery('villageInfo');
+    $query->where('objectId','55ae78fe00b096856ad76b35');
+    $return = $query->find();
+    print_r($return);
 ?>
 </body>
 </html>
