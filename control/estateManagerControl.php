c0bee2d93ee6ff5871cbe6a5082a91215b258712
muzhiqiang <694529694@qq.com>
2015-07-24 16:59:53 +0800

    third

--- control/estateManagerControl.php
+++ control/estateManagerControl.php
@@ -7,12 +7,15 @@ if(isset($_GET['method'])){
 
 	$estateManagerModel = new estateManagerModel();
 	$villageModel = new villageModel();
+
 	if($method=='getAll'){				//获得所有管理员信息
 		$estateManagerList = $estateManagerModel->getAll();
 		session_start();
 		$_SESSION['estateManagerList']=$estateManagerList;
 		header("Location:".__PUBLIC__."/view/admin/estateManagerIndex.php");
-	}else if($method=='register'){     //注册管理员账号
+	}
+
+	else if($method=='register'){     //注册管理员账号
 		$villageId = $_GET['id'];
 		if(!isset($_POST['username'])||!isset($_POST['password'])){
 			header("Location:".__PUBLIC__."/view/admin/registerManager.php?id=".$villageId);
@@ -23,22 +26,26 @@ if(isset($_GET['method'])){
 			header("Location:".__PUBLIC__.'/control/estateManagerControl.php?method=getAll');
 		}
 		
-	}else if($method=='look'){   		//查看管理员所属小区信息
+	}
+
+	else if($method=='look'){   		//查看管理员所属小区信息
 		$villageId = $_GET['id'];
 		$villageInfo = $villageModel->getByVillageId($villageId);
 		session_start();
 		$_SESSION['villageInfo'] = $villageInfo;
 		header("Location:".__PUBLIC__."/view/admin/lookVillage.php");
-	}else if($method=='login'){			//管理员登录处理
+	}
+
+	else if($method=='login'){			//管理员登录处理
 		if(isset($_POST['username'])&&isset($_POST['password'])){
 			$estateManager = $estateManagerModel->login($_POST['username'],$_POST['password']);
 			if(!empty($estateManager)){
 				$villageId = $estateManager['villageId'];				
 				$villageInfo = $villageModel->getByVillageId($villageId);
-                $estateManager = array_merge($estateManager[0],array('villageName'=>$villageInfo[0]['villageName']));
+                $estateManager = array_merge($estateManager,array('villageName'=>$villageInfo['villageName']));
 				session_start();
 				
-				$_SESSION['villageInfo'] = $villageInfo;
+				$_SESSION['estateManager'] = $estateManager;
 			}
 			
 				
