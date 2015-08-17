e66f282d72d6beeb6e187cdfb2547c1991645b9b
luoxiao <409494308@qq.com>
2015-08-17 16:33:10 +0800

    d

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php 
 	
-	//define('__PUBLIC__','http://207.226.143.137'.'/estateManage');
-	define('__PUBLIC__','http://localhost/estateManage');
+	define('__PUBLIC__','http://207.226.143.137'.'/estateManage');
+	//define('__PUBLIC__','http://localhost/estateManage');
 ?>
